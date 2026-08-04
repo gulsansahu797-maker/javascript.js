41200b8a5c2b63c61a94a5aacdbbc14a3e0898c6
Gulsan Shah <gulsansahu797@gmail.com>
2026-08-04 16:57:01 +0530

    Add initial versiofirstclass.jsn of firstclass.js with variable declarations and data types examples

---
+++ firstclass.js
@@ -0,0 +1,37 @@
+// // console.log("Hello JavaScript")
+
+// // use to let const and let to declare vribles
+
+// var age = 20;
+// console.log(age);
+
+// let city = "Delhi";
+// console.log(city);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// // Data Types
+// // Number
+
+// let age = 22;
+
+
+// String
+// let name = "gulsan";
+
+// Boolean
+// let isStudent = true; // true aur fakse
+
+// Undefined
+// let x;
+// console.log(x); // udefined
+
+// null
+// let y = null;
+// console.log(y); // null
+
+// typeof Operator
+// let age = 22;
+
+// console.log(typeof age);
