8b28b72850e56e8ea90584e51ce24af16ff4f545
Gulsan Shah <gulsansahu797@gmail.com>
2026-08-13 03:50:32 +0530

    /classes_and_oop/oop.js

---
+++ index.js/classes_and_oop/oop.js
@@ -0,0 +1,45 @@
+// function getUserDetails(user) {
+//     console.log("got user details");
+//     console.log('Username: ${this.username}');
+//     console.log(this);
+// }
+
+// const user = {
+//     username: "gulsan kumar",
+//     loginCount: 8,
+//     signedIn: true
+
+// }
+
+// getUserDetails(user);
+// //console.log("got user details");
+// // console.log('Username: ${this.username}');
+// console.log(this);
+
+// console.log(user.username);
+// //console.log(user.getUserDetails());
+// console.log(this);
+
+
+// function User(username, loginCount, signedIn) {
+//     this.username = username;
+//     this.loginCount = loginCount;
+//     this.signedIn = signedIn;
+
+//     return this
+// }
+
+// const user1 = new User("gulsan kumar", 8, true);
+// console.log(user1);
+
+// const user2 = new User("vinod sahu", 25, false);
+// console.log(user2);
+
+// const user3 = new User("sandeepkumar", 15, true);
+// console.log(user3);
+
+// const user4 = new User("gautamsahu", 10, true);
+// console.log(user4);
+
+// const user5 = new User("ashish shah", 17, true);
+// console.log(user5);
